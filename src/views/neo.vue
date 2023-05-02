105dffe2cf1383f7c59a750aa2ac517123c0b538
John Slowik <john@jslowik.com>
2023-05-02 16:34:36 -0500

    Icon to indicate external link

--- src/views/neo.vue
+++ src/views/neo.vue
@@ -29,7 +29,23 @@
 				<tbody class="divide-y divide-gray-200 bg-white">
 					<tr v-for="neo in neoModel.data?.near_earth_objects" :key="neo.id">
 						<td class="px-3 py-4 text-sm text-gray-500 lg:table-cell">
-							<a :href="neo.nasa_jpl_url" target="_blank">{{ neo.name }}</a>
+							<a :href="neo.nasa_jpl_url" target="_blank">
+								{{ neo.name }}
+								<svg
+									xmlns="http://www.w3.org/2000/svg"
+									fill="none"
+									viewBox="0 0 24 24"
+									stroke-width="1.5"
+									stroke="currentColor"
+									class="inline h-4 w-4"
+								>
+									<path
+										stroke-linecap="round"
+										stroke-linejoin="round"
+										d="M13.5 6H5.25A2.25 2.25 0 003 8.25v10.5A2.25 2.25 0 005.25 21h10.5A2.25 2.25 0 0018 18.75V10.5m-10.5 6L21 3m0 0h-5.25M21 3v5.25"
+									/>
+								</svg>
+							</a>
 						</td>
 						<td class="px-3 py-4 text-sm text-gray-500 lg:table-cell">{{ neo.id }}</td>
 						<td class="px-3 py-4 text-sm text-gray-500 sm:table-cell">
